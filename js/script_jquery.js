d698890c3db8a329992832a87eb5166a72223f3a
arpit625 <arpit625@gmail.com>
2016-05-03 18:20:02 +0530

    AJAX Implementation

--- js/script_jquery.js
+++ js/script_jquery.js
@@ -1,11 +1,20 @@
+var allData;
+
 $( document ).ready(function() {
 
 	console.log( "ready123!" );
 
-	$(".rowTopNumber").html("<h3>349</h3>");
+	// $(".rowTopNumber").html("<h3>349</h3>");
 
-    callAjax(servervalue);
+	callAjax();
 
+	/*
+	Below line won't work as it gets executed before the 'allData' variable gets vaule in it
+	JS doesn't wait for the function to finish and keep moving on
+	To handle this use the globar variable with allData after the AJAX call and make it like a chain of 
+	Document.ready -> callAJAX -> function()
+	*/
+	// $("#testing").text("Welcome "+allData.Discount);
 
 	columnchart("#chart1");
 	barchart("#chart2");
@@ -14,7 +23,33 @@ $( document ).ready(function() {
 
 });
 
-function callAjax() {
 
-	
+function callAjax() {
+	$.ajax({
+		type: 'GET',
+		url: "server/data_process.php",
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json'
+    }).done(function (dt) {
+        	allData = dt;
+
+            // $("#testing").text("Welcome "+dt.Sales);
+			// $("#testing").text("Welcome "+allData.Discount);
+
+			setValues();
+
+    }).fail(function (dt) {
+    	$("#testing").text("Failed");
+
+
+
+    });
+}
+
+function setValues() {
+	$(".rowTopNumber1").text(allData.Sales);
+	$(".rowTopNumber2").text(allData.Quantity);
+	$(".rowTopNumber3").text(allData.Discount);
+	$(".rowTopNumber4").text(allData.Profit);
+
 }
--- server/data_process.php
+++ server/data_process.php
@@ -10,9 +10,10 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully <br>";
+// echo "Connected successfully <br>";
 
 
+/*// Get processed data
 $sql = "SELECT segment, sales FROM test.sales limit 10";
 $result = $conn->query($sql);
 
@@ -23,8 +24,26 @@ if ($result->num_rows > 0) {
     }
 } else {
     echo "0 results";
+}*/
+
+$final = array();
+
+// Get processed data
+$sql = "Select 	SUM(sales) as Sales, SUM(Quantity) as Quantity, AVG(Discount) as Discount, SUM(Profit) as Profit from test.sales";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+        $final = array('Sales' => $row["Sales"], 'Quantity' =>  $row["Quantity"], 'Discount' => $row["Discount"], 'Profit' => $row["Profit"]);
+    }
+} else {
+    echo "No";
 }
 
+// echo ($final);
+echo json_encode($final);
 
 $conn->close();
 
